cabc3d69adf2e78d7f7b0c8eac5b3d61112e68d2
suryanbhatia <sbhatia965@student.fuhsd.org>
2019-03-10 18:57:54 -0700

    Created State Class

--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,10 @@
 public class Main {
     public static void main(String[] args) {
         //Test of Utils
-
-        String data = Utils.readFileAsString("data/2016_Presidential_Results.csv");
-        System.out.println(data);
+        /*String data = Utils.readFileAsString("data/2016_Presidential_Results.csv");
+        System.out.println(data);*/
+        Utils test = new Utils();
+        String data2016 = test.readFileAsString("data/2016_Presidential_Results.csv");
+        System.out.println(test.parse2016ElectionResults(data2016));
     }
 }
